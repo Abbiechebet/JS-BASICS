d690ae1873e11f4fdcc1b47e961c28b318baff01
Abbiechebet <abigaelchebet85@gmail.com>
2023-02-10 10:14:53 +0300

    more changes

---
+++ index.js
@@ -0,0 +1,33 @@
+//function that multiplies two numbers
+function multiply (number1, number2) {
+    return number1 * number2;
+}
+console.log(multiply(123, 48))
+
+/** 
+ * create variables for your first name, last name, Stutern track, skills, favourite color,
+   year of your country’s independence
+ */
+let firstName='Abigael';
+let lastName='Chebet';
+let StuternTrack='Software Development';
+let skills='Git and Github, HTML, CSS, Javascript';
+let favoriteColor='pink';
+let KenyaIndependenceYear='1963';
+
+ /**Use these variables to form a sentence 
+and log the sentence in the console
+*/
+console.log(firstName);
+console.log(lastName);
+console.log(StuternTrack);
+console.log(skills);
+console.log(favoriteColor);
+console.log(KenyaIndependenceYear);
+
+//concatenation
+console.log("My name is " + firstName +" " + lastName + 
+". I am in Stutern track " + StuternTrack + " where I have acquired skills in " 
++ skills + ". My favorite color is " +  favoriteColor +
+". My country Kenya got her independence in the year "
+ + KenyaIndependenceYear + ".")
